b806911c3334c7aab55e078a337391f098025352
MERCUROUS <sameershekhar100@gmail.com>
2024-06-04 01:51:21 +0530

    updated Colors

--- app/src/main/java/com/example/projectevovlution/ui/screen/calculatorscreen/InputKeyView.kt
+++ app/src/main/java/com/example/projectevovlution/ui/screen/calculatorscreen/InputKeyView.kt
@@ -1,6 +1,5 @@
 package com.example.projectevovlution.ui.screen.calculatorscreen
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,14 +11,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.projectevovlution.ui.route.Screen
+import com.example.projectevovlution.R
 
 @Composable
 fun InputKeyView() {
@@ -49,7 +47,7 @@ fun CreateInputText(input: String) {
             modifier = Modifier
                 .drawBehind {
                     drawCircle(
-                        color = Color.LightGray,
+                        color = Color(R.color.light_grey),
                         radius = 98.0f
                     )
                 },
