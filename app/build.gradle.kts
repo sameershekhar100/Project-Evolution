373aabdcfa8fef6f8b66603efa740175a68b4e88
MERCUROUS <sameershekhar100@gmail.com>
2024-09-25 04:22:51 +0530

    Updated module name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":basicnote_1"))
+    implementation(project(":notes"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
