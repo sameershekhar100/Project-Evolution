5a0cad79836c12d68e6fdffe1b10e3d21ea4e164
MERCUROUS <sameershekhar100@gmail.com>
2024-09-27 04:26:44 +0530

    Added Retrofit Dependency and Response Class

--- news/build.gradle.kts
+++ news/build.gradle.kts
@@ -67,5 +67,11 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-name:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation(libs.gson)
 }
