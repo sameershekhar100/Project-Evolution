3ae26a597fae91a1f5eddb1d09373be95db0adb2
MERCUROUS <sameershekhar100@gmail.com>
2024-09-27 04:33:56 +0530

    Lint fix

--- news/build.gradle.kts
+++ news/build.gradle.kts
@@ -68,9 +68,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-name:2.9.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation(libs.gson)
