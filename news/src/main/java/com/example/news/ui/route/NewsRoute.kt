d11617929591321e6b6751832d8d39d02de1a612
MERCUROUS <sameershekhar100@gmail.com>
2024-10-12 17:39:33 +0530

    Fixed Bottom Nav Issues.

--- news/src/main/java/com/example/news/ui/route/NewsRoute.kt
+++ news/src/main/java/com/example/news/ui/route/NewsRoute.kt
@@ -1,32 +1,28 @@
 package com.example.news.ui.route
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.outlined.List
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import com.example.news.R
 
 sealed class NewsRoute(
     val route: String,
     val title: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector
+    val selectedIcon: Int,
+    val unselectedIcon: Int
 ) {
     data object Home : NewsRoute(
         route = "Home",
         title = "Headlines",
-        selectedIcon = Icons.Filled.Star,
-        unselectedIcon = Icons.Outlined.Star
+        selectedIcon = R.drawable.baseline_star_24,
+        unselectedIcon = R.drawable.baseline_star_outline_24
     )
 
     data object Saved : NewsRoute(
         route = "Saved",
         title = "Saved",
-        selectedIcon = Icons.Filled.List,
-        unselectedIcon = Icons.Outlined.List
+        selectedIcon = R.drawable.baseline_save_24,
+        unselectedIcon = R.drawable.outline_save_24
 
     )
 }
--- news/src/main/java/com/example/news/ui/screens/NewsScreen.kt
+++ news/src/main/java/com/example/news/ui/screens/NewsScreen.kt
@@ -1,10 +1,7 @@
 package com.example.news.ui.screens
 
 import android.annotation.SuppressLint
-import android.util.Log
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.outlined.Star
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -12,35 +9,21 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.news.R
 import com.example.news.ui.navgraphs.HomeNavGraph
 import com.example.news.ui.route.NewsRoute
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun NewsScreen(navController: NavHostController = rememberNavController()) {
-    val items = listOf(
-        BottomNavigationItem(
-            title = "Headlines",
-            selectedIcon = Icons.Filled.Star,
-            unselectedIcon = Icons.Outlined.Star
-        ), BottomNavigationItem(
-            title = "Saved",
-            selectedIcon = ImageVector.vectorResource(id = R.drawable.baseline_save_24),
-            unselectedIcon = ImageVector.vectorResource(id = R.drawable.outline_save_24)
-        )
-    )
-    Scaffold(
-        bottomBar = { CreateBottomBar(navController) }
-    )
+    Scaffold(bottomBar = { CreateBottomBar(navController) })
     {
         HomeNavGraph(navController = navController)
     }
@@ -48,41 +31,61 @@ fun NewsScreen(navController: NavHostController = rememberNavController()) {
 
 @Composable
 fun CreateBottomBar(navController: NavHostController) {
-    val items = listOf(NewsRoute.Home, NewsRoute.Saved)
+    val items = listOf(
+        NewsRoute.Home,
+        NewsRoute.Saved
+    )
+
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
-    Log.v("zzzz",currentDestination?.route.toString())
-    val bottomBarDestination = items.any { it.route == currentDestination?.route }
-    var selectedItem by rememberSaveable { mutableIntStateOf(0) }
-    if(bottomBarDestination) {
-        NavigationBar {
-            items.forEachIndexed { index, item ->
-                NavigationBarItem(
-                    selected = selectedItem == index,
-                    onClick = {
-                        selectedItem = index
-                        navController.navigate(item.route) {
-                            launchSingleTop = true
-                        }
-                        //navigation.navigate(item.title)
-                    },
-                    label = { Text(text = item.title) },
-                    icon = {
-                        Icon(
-                            imageVector = (if (selectedItem == index) item.selectedIcon else item.unselectedIcon),
-                            contentDescription = item.title
-                        )
-                    })
+
+    NavigationBar {
+        CreateBottomNavItem(items, currentDestination, navController)
+    }
+
+}
+
+@Composable
+fun RowScope.CreateBottomNavItem(
+    items: List<NewsRoute>,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    items.forEach { item ->
+        NavigationBarItem(
+            selected = (currentDestination?.hierarchy?.any {
+                it.route == item.route
+            } == true),
+
+            onClick = {
+                navController.navigate(item.route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            },
+            label = { Text(text = item.title) },
+            icon = {
+                Icon(
+                    imageVector = (
+                            if (currentDestination?.route == item.route)
+                                getIconFromDrawable(res = item.selectedIcon)
+                            else getIconFromDrawable(res = item.unselectedIcon)
+                            ),
+                    contentDescription = item.title
+                )
             }
-        }
+        )
     }
 }
 
-data class BottomNavigationItem(
-    val title: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector
-)
+
+@Composable
+fun getIconFromDrawable(res: Int): ImageVector {
+    return ImageVector.vectorResource(id = res)
+}
 object Graph {
     const val Root = "root_graph"
 }
